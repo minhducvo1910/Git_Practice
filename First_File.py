25140697174870d535315fa490a9ccadb1e6ed1c
Minh Duc Vo <paulthimony@gmail.com>
2026-05-21 09:58:34 -0500

    This is the 4th commit after adding more code and branch file

--- First_File.py
+++ First_File.py
@@ -3,3 +3,18 @@
 
 #This is my code
 print("Hello World!")
+
+#This is a change to the code
+class dog():
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+    def bark(self):
+        print(f"{self.name} says Woof!")
+    def info(self):        
+        print(f"{self.name} is a {self.breed}.")
+        
+name = input("Enter your dog's name: ")
+my_dog = dog(name, "Golden Retriever")
+my_dog.bark()
+my_dog.info()
